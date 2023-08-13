fe9539dbd938b15bdc9276a30817dc0f0e4d6cd3
Song Sung Woo <59758042+songsungwoo97@users.noreply.github.com>
2023-08-14 02:19:16 +0900

    Add files via upload

---
+++ asac_week3/asac_week3/src/main/java/com/example/asac_week3/domain/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.example.asac_week3.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
