4cbbd9911fa59014edcb0d223264ecc6fa87f831
Song Sung Woo <59758042+songsungwoo97@users.noreply.github.com>
2023-08-14 02:22:26 +0900

    Delete asac_week3/asac_week3 directory

--- asac_week3/asac_week3/src/main/java/com/example/asac_week3/domain/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.asac_week3.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
