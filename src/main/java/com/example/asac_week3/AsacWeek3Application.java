d6d0b634ca30e0da4cfc19952a4251f2873d52e5
songsungwoo <sqwa9200@naver.com>
2023-08-14 17:04:16 +0900

    solve error3

--- src/main/java/com/example/asac_week3/AsacWeek3Application.java
+++ src/main/java/com/example/asac_week3/AsacWeek3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class git AsacWeek3Application {
+public class AsacWeek3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsacWeek3Application.class, args);
